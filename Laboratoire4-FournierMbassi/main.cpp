6412886cfe6222e978211caa42297d0ff1a50bdc
Dwimcore <fournier.robin@gmail.com>
2016-10-12 16:55:20 +0200

    Initial commit

---
+++ Laboratoire4-FournierMbassi/main.cpp
@@ -0,0 +1,28 @@
+/*
+ -----------------------------------------------------------------------------------
+ Laboratoire : 04
+ Fichier     : Fournier_Mbassi.cpp
+ Auteur(s)   : Robin Fournier
+ Date        : 12.10.2016
+
+ But         : - Instructions conditionnelles
+               - Vérification des valeurs saisies
+
+ Remarque(s) : <à compléter>
+
+ Compilateur : Ubuntu g++ 5.4.0
+ -----------------------------------------------------------------------------------
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
