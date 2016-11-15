7b3c4e1d7106411a67e8d1216feb65cdbd595140
Danattar <danattarxelnaga@gmail.com>
2016-11-15 16:30:40 +0100

    Update KorradiGottburg.cpp

--- Laboratoire6/KorradiGottburg.cpp
+++ Laboratoire6/KorradiGottburg.cpp
@@ -3,15 +3,13 @@
 Laboratoire  : Laboratoire 06
 Fichier      : KorradiGottburgLabo6.cpp
 
-Auteurs      : - Michael Gottburg (1INF1)
-			   - Frederic Korradi (1INF1)
+Auteurs      : - Michael Gottburg (INF1)
+			   - Frederic Korradi (INF1)
 
-Dates        : - 29.10.2016 (debut du projet)
-			   - 10.11.2016 (rendu du projet)
+Dates        : - 15.11.2016 (debut du projet)
+			   - 15.11.2016 (rendu du projet)
 
 Buts         : - Appliquer les elements vus dans les chapitres 3a et 3b
-			   - Utiliser les chaines de caracteres en tant que tableau
-			   - Calcul binaire, conversion, utilision des casts, etc.
 
 Remarque(s)  : std::cout car sous linux on peut seulement faire les appels
 			   de cette manière.
@@ -30,31 +28,36 @@ int main() {
 	string output="";
 	int lastInput = 0;
 	cout << "Valeurs : ";
-
-	while (input) {
+	int compteur = 0;
+	while (input !=0) {
 		cin >> input;
-		int compteur = 0;
+		if(cin.fail()) {  // Pour gérer les erreurs de saisie, on kill le programme.
+			input = 0;
+		}
 		if (input == lastInput) {
 			compteur++;
 		}
-		if (input < lastInput) {
-			croissante = false;
-		}
-		if (compteur) {
-			cout << compteur;
-			cout << "x";
-		}
 		else {
-			cout << input;
-			cout << ",";
+			if (input < lastInput && input) {
+				croissante = false;
+			}
+			if (compteur) {
+				cout << ++compteur;
+				cout << "x";
+				compteur = 0;
+			}if (!compteur && lastInput) {
+				cout << lastInput;
+				if (input) {
+					cout << ",";
+				}
+			}
 		}
 		lastInput = input;
 	}
-	cout << "Suite ";
+	cout << endl << "Suite";
 	if(!croissante){
-		cout << "non";
+		cout << " non";
 	}
 	cout << " croissante" << endl;
-
 	return 0;
 }
