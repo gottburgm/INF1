f18d240df8ae6b42f1de4b9f4073265bc7386bea
gottburgm <mikagott@hotmail.com>
2016-11-16 13:44:01 +0100

    s

--- Laboratoire6/KorradiGottburg.cpp
+++ Laboratoire6/KorradiGottburg.cpp
@@ -1,24 +1,4 @@
 /*
-<<<<<<< HEAD
----------------------------------------------------------------------------------
-Laboratoire  : Laboratoire 06
-Fichier      : KorradiGottburgLabo6.cpp
-
-Auteurs      : - Michael Gottburg (INF1)
-				- Frederic Korradi (INF1)
-
-Dates        : - 15.11.2016 (debut du projet)
-				- 15.11.2016 (rendu du projet)
-
-Buts         : - Appliquer les elements vus dans les chapitres 3a et 3b
-
-Remarque(s)  : std::cout car sous linux on peut seulement faire les appels
-				de cette manière.
-Compilateurs : - MinGW-g++ 4.8.1 (Windows)
-				- gcc version 4.9.2 (Linux)
----------------------------------------------------------------------------------
-*/
-=======
  -------------------------------------------------------------------------------
  Laboratoire  : Laboratoire 06
  Fichier      : KorradiGottburgLabo6.cpp
@@ -38,47 +18,11 @@ Compilateurs : - MinGW-g++ 4.8.1 (Windows)
  -------------------------------------------------------------------------------
  */
 
->>>>>>> 5410a08df271e243174cd30bb55426f2b15d8562
 #include <iostream>
 
 using namespace std;
 
 int main() {
-<<<<<<< HEAD
-  int input = 1;
-  int lastInput = 0;
-  int compteur = 0;
-  bool croissante = true;
-  string croissante = "croissante";
-  
-  cout << "Valeurs : ";
-  while (input) {
-    cin >> input;
-    if(cin.fail()) {  // Pour gérer les erreurs de saisie, on kill le programme.
-      continue;
-    } else if (lastInput) {
-      if (input == lastInput) {
-	compteur++;
-      } else if (input < lastInput && input) {
-	croissante = "non croissante";
-      }
-      if (compteur) {
-	cout << ++compteur;
-	cout << "x";
-	compteur = 0;
-      } else {
-	cout << lastInput;
-	if (input) {
-	  cout << ",";
-	}
-      }
-    }
-    lastInput = input;
-  }
-  cout << endl << "Suite " << croissante << endl;
-  
-  return 0;
-=======
     int input = 1;
     int inputCount = 0;
     int lastInput = 0;
@@ -122,5 +66,4 @@ int main() {
         cout << endl << (inputCount ? "Une seule valeur saisie ." : "Aucune valeur saisie .") << endl;
     }
     return 0;
->>>>>>> 5410a08df271e243174cd30bb55426f2b15d8562
 }
