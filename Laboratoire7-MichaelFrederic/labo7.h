a3e66a3e550159deed0ea1bb4581e8dc1cb60e04
gottburgm <mikagott@hotmail.com>
2016-11-23 17:39:22 +0100

    a

--- Laboratoire7-MichaelFrederic/labo7.h
+++ Laboratoire7-MichaelFrederic/labo7.h
@@ -1,265 +1,81 @@
-/*
- -----------------------------------------------------------------------------------
- Laboratoire  : Labo 007
- Fichier      : 23_11_2016_17_10.cpp
- Auteur(s)    : Michael Gottburg
- Classe       : 1NF1
- Date         : 23.11.2016
+/**
+ \brief Fonction exercice1 : <description>
 
+ \details La fonction  a pour but de <description detaillée>
 
- But          : 
-
-
- Remarque(s)  : std::cout car sous linux on peut seulement faire l'appel de cout
-		de cette manière.
-
- Compilateurs : Linux (g++ (Debian 4.9.2-10) 4.9.2) / Windows (MinGW-g++ 4.8.1)
- -----------------------------------------------------------------------------------
 */
 
 
-/* Librairies */
-#include <cstdlib>
-#include <iostream>
-#include <iomanip>
-#include <string>
-#include <climits>
-#include <cfloat>
-#include <cmath>
-
-using namespace std;
-
-
-
-
-
-/* Prototype de la fonction exercice1_main */
-void exercice1_main ();
+void exercice1();
 
 /**
- \brief Fonction exercice1_main : <description>
-
+ \brief Fonction exercice2 : <description>
 
  \details La fonction  a pour but de <description detaillée>
 
 */
 
 
-void exercice1_main ()
-{
-	/* Declaration des variables locales de exercice1_main */
-
-    bool is_perfect_square;
-
-
-	/* --------- Code --------- */
-
-    10;
-    1
-    1
-    1
-}
-
-
-/* Prototype de la fonction exercice2_main */
-void exercice2_main ();
+void exercice2();
 
 /**
- \brief Fonction exercice2_main : <description>
-
+ \brief Fonction exercice3 : <description>
 
  \details La fonction  a pour but de <description detaillée>
 
 */
 
 
-void exercice2_main ()
-{
-	/* Declaration des variables locales de exercice2_main */
-
-     input = 0;
-
-
-	/* --------- Code --------- */
-
-    1:displayDiamond(input);
-    11;
-    1
-}
-
-
-/* Prototype de la fonction exercice3_main */
-void exercice3_main ();
+void exercice3();
 
 /**
- \brief Fonction exercice3_main : <description>
-
+ \brief Fonction exercice4 : <description>
 
  \details La fonction  a pour but de <description detaillée>
 
 */
 
 
-void exercice3_main ()
-{
-	/* Declaration des variables locales de exercice3_main */
-
-
-
-	/* --------- Code --------- */
-
-}
-
-
-/* Prototype de la fonction exercice4_main */
-void exercice4_main ();
+void exercice4();
 
 /**
- \brief Fonction exercice4_main : <description>
-
+ \brief Fonction exercice5 : <description>
 
  \details La fonction  a pour but de <description detaillée>
 
 */
 
 
-void exercice4_main ()
-{
-	/* Declaration des variables locales de exercice4_main */
-
-
-
-	/* --------- Code --------- */
-
-    1:displayFibonacci(input)
-    12;
-    1
-}
-
-
-/* Prototype de la fonction exercice5_main */
-void exercice5_main ();
+void exercice5();
 
 /**
- \brief Fonction exercice5_main : <description>
-
+ \brief Fonction afficher : <description>
 
  \details La fonction  a pour but de <description detaillée>
 
 */
 
 
-void exercice5_main ()
-{
-	/* Declaration des variables locales de exercice5_main */
-
-    string str_input;
-     nbChar = 0;
-     nbWords = 0;
-     wordMaxLength = 0;
-     nbNonAlphaNumChars = 0;
-
-
-	/* --------- Code --------- */
-
-}
-
-
-/* Prototype de la fonction  */
-  ();
+void afficher();
 
 /**
- \brief Fonction  : <description>
-\return  is_perfect_square : variable de retour de la fonction
-
-
- \details La fonction  a pour but de <description detaillée>
-
-*/
-
-
-  ()
-{
-	/* Declaration des variables locales de  */
-
-
-
-	/* --------- Code --------- */
-
-
-
-
-	return is_perfect_square;
-}
-
-
-/* Prototype de la fonction displayDiamond */
-void displayDiamond ( int input) {;
-
-\param[in]  : Paramètre d'entrée de type 
-
+ \brief Fonction displayDiamond : <description>
+ \param[in] int input : Paramètre d'entrée de type 
 
  \details La fonction  a pour but de <description detaillée>
 
 */
 
 
-void displayDiamond ( int input) {
-{
-	/* Declaration des variables locales de displayDiamond */
+void displayDiamond(  int input);
 
-
-
-	/* --------- Code --------- */
-
-}
-
-
-/* Prototype de la fonction displayFibonacci */
-void displayFibonacci ( int input) {;
-
-\param[in]  : Paramètre d'entrée de type 
-
+/**
+ \brief Fonction displayFibonacci : <description>
+ \param[in] int input : Paramètre d'entrée de type 
 
  \details La fonction  a pour but de <description detaillée>
 
 */
 
 
-void displayFibonacci ( int input) {
-{
-	/* Declaration des variables locales de displayFibonacci */
-
-
-
-	/* --------- Code --------- */
-
-}
-
-
-/* Prototype de la fonction isPerfectSquare */
-bool isPerfectSquare ( int input) {;
-
-\param[in]  : Paramètre d'entrée de type 
-\return  is_perfect_square : variable de retour de la fonction
-
-
- \details La fonction  a pour but de <description detaillée>
-
-*/
-
-
-bool isPerfectSquare ( int input) {
-{
-	/* Declaration des variables locales de isPerfectSquare */
-
-
-
-	/* --------- Code --------- */
-
-
-
-
-	return is_perfect_square;
-}
-
+void displayFibonacci(  int input);
