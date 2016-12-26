426bf0417949344c3c7a76345fce5af191a67292
Danattar <danattarxelnaga@gmail.com>
2016-12-26 17:11:02 +0100

    Update FournierMbassilabo8_funcs.cpp

--- Laboratoire8-FournierMbassi_2/FournierMbassilabo8_funcs.cpp
+++ Laboratoire8-FournierMbassi_2/FournierMbassilabo8_funcs.cpp
@@ -1,113 +1,194 @@
 /**
- -----------------------------------------------------------------------------------
- \file      FournierMbassi_labo8_funcs.cpp
- \brief     Laboratoire 8 - Fonction et Tableau à 1 et 2 dimensions
- \authors   Vladimir Mbassi\n
-            Robin Fournier
- \date      20.12.2016
- \details   Fichier de définition des fonctions
-  
- -----------------------------------------------------------------------------------
- */
+* \file KorradiGottburgFunctions.cpp
+* \brief Definition des fonctions
+* \details Definition des prototypes contenus dans le fichier Functions.h
+* \author Frederic Korradi
+* \author Michael Gottburg
+* \version 1.0
+* \date 2016-12-20
+* \copyright GNU Public License and HEIG-VD
+*/
 
-#include "FournierMbassilabo8_funcs.h"
+#include "KorradiGottburgFunctions.h"
 
-using namespace std;
 
-void copier(const int tab1[], int tab2[], size_t N, size_t& taille2) {
-    for (int i = 0; i < N; i++) {
-        tab2[i] = tab1[i];
-    }
-    taille2 = CAPACITE; // On réinitialise la taille de table2 à CAPACITE
+bool inRange(int value, int min, int max) {
+	return (value >= min && value <= max ? true : false);
 }
 
-void afficher(const int tab[], size_t N) {
-    cout << "[";
-    for (int i = 0; i < N - 1; i++) {
-        cout << tab[i] << ",";
-    }
-    cout << tab[N - 1] << "]" << endl;
+int getInput(const string& MESSAGE, int max, int min) {
+	int value = 0;
+	cout << MESSAGE;
+
+	while (!(cin >> value) || !(inRange(value, min, max))) {
+		cin.clear();
+		cin.ignore(numeric_limits<int>::max(), '\n');
+		cout << MESSAGE;
+	}
+	cin.ignore(numeric_limits<int>::max(), '\n');
+	return value;
 }
 
-void suppr_doublon(int tab[], size_t& taille) {
-    for (int i = 0; i < taille; i++) {  // Pour chaque valeur du tableau
-        for (int j = i + 1; j < taille; j++) { // Verification de tous les val suivantes
-            if (tab[j] == tab[i]) {
-                // Déclalage à gauche si doublon
-                for (int k = j; k < taille; k++) {
-                    tab[k] = tab[k + 1];
-                }
-                taille--;
-                i--;    // Décrementation nécessaire si multiples doublons successifs
-            }
-        }
-    }
+void generateTab1(int tab1[], size_t& size) {
+	for (size_t index = 0; index < size; index++) {
+		tab1[index] = rand() % 10;
+	}
+	return;
 }
 
-void rand_initialisation(int tab[], size_t N) {
-    const int MAX = 10, MIN = 0;
-    for (int i = 0; i < N; i++) { // Affectation d'une val aléatoire entre MAX et MIN
-        tab[i] = (rand() % (MAX - MIN + 1)) + MIN;
-    }
+void copyTab1InTab2(const int tab1[], int tab2[], size_t& size_tab1, size_t& size_tab2) {
+	size_tab2 = 0;
+	for (size_t index = 0; index < size_tab1; index++) {
+		tab2[index] = tab1[index];
+		size_tab2++;
+	}
+
+	return;
 }
 
-void insertion(int tab[], size_t& taille) {
-    for (int i = 0; i < taille; i++) {
-        if (tab[i] % 2 == 0) {  // Si valeur paire
-            if (taille < CAPACITE) {    // Augmentation de la taille si inférieur a la capacité
-                taille++;
-            }
-            // On décale toutes les valeurs à droite après "i"
-            for (int j = taille - 1; j > i + 1; j--) {
-                tab[j] = tab[j - 1];
-            }
-            // Puis on insère valeur + 1 à "i + 1"
-            tab[i + 1] = tab[i] + 1;
+void printTab(const int tab[], size_t& size, const string& TEXTE) {
+	cout << TEXTE << endl;
+	for (size_t index = 0; index < size; index++) {
+		cout << tab[index] << ' ';
+	}
+	cout << endl;
 
-
-        }
-    }
+	return;
 }
 
-void initialisation_2d(int tab2d[][N_COLONNES], const int tab[], size_t N) {
-    if (N_COLONNES * N_LIGNES >= N) {   // Verification de la taille du tableau 2D
-        int j = 0, k = 0;
-        for (int i = 0; i < N; i++) {
-            tab2d[j][k] = tab[i];
-            if (k < N_COLONNES - 1) {
-                k++;
-            } else {
-                k = 0;
-                j++;
-            }
-        }
-    } else {    // Table 2D trop petite -> Message d'erreur
-        cout << "table3 est trop petit par rapport a table1" << endl;
-    }
+void deleteDuplicatesTab2(int tab2[], size_t& size) {
+	const short INC = 1;
+	const short DOUBLEINC = 2 * INC;
+	unsigned int counter = 0;
+	if (size) {
+		for (size_t index = 0; index < size; index++) {
+			for (size_t index2 = index + INC; index2 < size-INC; index2++) {
+				if (tab2[index] == tab2[index2]) {
+					for (size_t index3 = index2 + INC; index3 < size + DOUBLEINC; index3++) {
+						tab2[index3-INC] = tab2[index3];
+					}		
+					counter++;
+				}
+			}
+		}
+		size -= counter;
+	}
+	return;
 }
 
-void afficher_2d(const int tab2d[][N_COLONNES]) {
-    for (int i = 0; i < N_LIGNES; i++) {
-            afficher(tab2d[i], N_COLONNES);
-        }
+void insertInTab2IncrementedValuesAfterPairValues(int tab2[], size_t& size_tab_2, const unsigned int& CAPACITYTAB1AND2) {
+	const short DIVIDER = 2;
+	const short INC = 1;
+	const string ALERT = "ERREUR : Capacite du tableau depassee.";
+	for (size_t index = 0; index < size_tab_2 - INC; index++) {
+		if (size_tab_2 < CAPACITYTAB1AND2) {
+			if (tab2[index] % DIVIDER == 0) {
+				size_tab_2++;
+				for (size_t index2 = size_tab_2; index2 > index; index2--) {
+					tab2[index2] = tab2[index2 - INC];
+				}
+				tab2[index + INC] = tab2[index] + INC;
+				index++;
+			}
+		}
+		else {
+			cout << ALERT << endl;
+		}
+	}
+	return;
 }
 
-void tri_selection(int tab[], size_t N) {
-    int min;    // Indice de la contenant la valeur minimale actuelle
-    int tmp;    // Variable tampon pour l'échange entre tab[min] et tab[i]
-    
-    for (int i = 0; i < N - 1; i++) {   // Pour chaque valeur de tab[] sauf dernière
-        min = i;
-        for (int j = i + 1; j < N; j++) {   // On recherche la plus petite valeure
-            if (tab[j] < tab[min]) {
-                min = j;
-            }
-        }
-        if (min != i) { // On échange les valeurs de tab[min] et tab[i]
-            tmp = tab[i];
-            tab[i] = tab[min];
-            tab[min] = tmp;
-        }
-    }
+void initializeTab3WithTab1AndCompareThem(const int tab1[], size_t& size_tab1, int tab3[][6], size_t& size_tab3_rows, size_t& size_tab3_columns) {
+	unsigned last = 0;
+	size_t row = 0;
+	if (size_tab3_columns * size_tab3_rows >= size_tab1) {   // Verification de la taille du tableau 2D
+		int j = 0, k = 0;
+		for (int i = 0; i < size_tab1; i++) {
+			tab3[j][k] = tab1[i];
+			if (k < size_tab3_columns - 1) {
+				k++;
+			}
+			else {
+				k = 0;
+				j++;
+			}
+		}
+		for (int index = 0; index < size_tab3_rows; index++) {
+			printTab(tab3[index], size_tab3_columns, "Affichage du tableau 3 : ");
+		}
+	}
+	else {    // Table 2D trop petite -> Message d'erreur
+		cout << "table3 est trop petit par rapport a table1" << endl;
+	}
 }
 
+
+/*
+	for (size_t col = 0; col < size_tab3_columns; col++) {
+		for (row = 0; row < size_tab3_rows; row++) {
+			if (last + row <= size_tab1) {
+				tab3[col][row] = tab1[last + row];
+				cout << col << "," << row << " : " << tab3[col][row] << endl;
+			}
+			else {
+				tab3[col][row] = 11;
+				cout << col << "," << row << " : " << tab3[col][row] << endl;
+			}
+		}
+		last = row;
+		if (last + size_tab3_columns + 1 < size_tab1) {
+			size_tab3_columns++;
+		}
+	}
+
+	return;
+}
+
+*/
+
+void switchPos(int tab[], int x, int y) {
+	int temp = tab[x];
+	tab[x] = tab[y];
+	tab[y] = temp;
+
+	return;
+}
+
+int maxPos(int tab[], int size) {
+	int pos = 0;
+	int max_pos = 0;
+
+	while (pos < size) {
+		if (tab[pos] > tab[max_pos]) {
+			max_pos = pos;
+			pos++;
+		}
+	}
+
+	return max_pos;
+}
+
+void triSelection(int tab[], size_t& size) {
+	const short INC = 1;
+	int min;
+	int index_min;
+	int temp;
+
+	for (size_t index = 0; index < size - INC; index++) {
+		min = tab[index];
+		index_min = index;
+		for (size_t index2 = index + INC; index2 < size; index2++) {
+			if (tab[index2] < min) {
+				min = tab[index2];
+				index_min = index2;
+			}
+		}
+		if (index_min != index) {
+			temp = tab[index];
+			tab[index] = tab[index_min];
+			tab[index_min] = temp;
+		}
+	}
+
+	return;
+}
